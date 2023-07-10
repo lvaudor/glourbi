f798ec9293f577fa238259c36cf5c92decad86eb
lvaudor <lise.vaudor@ens-lyon.fr>
2023-07-10 14:37:02 +0200

    Continent North America (AN)

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,12 +2,14 @@ library(tidyverse)
 ## code to prepare `DATASET` dataset goes here
 all_cities=read_csv("data-raw/GHS_all_complete_subset.csv")
 all_cities=all_cities %>%
+  mutate(Continent=case_when(is.na(Continent)~"AN",
+                              TRUE~Continent)) %>%
   group_by(Urban.Aggl) %>%
   mutate(ntot=n()) %>%
   mutate(rank=1:n()) %>%
   mutate(rank=case_when(ntot==1~"",
                         TRUE~as.character(rank))) %>%
-  mutate(name=paste0(Urban.Aggl,rank)) %>%
+  mutate(name=paste0(Urban.Aggl,"-", rank,"-",Country.or)) %>%
   select(-Urban.Aggl,-ntot,-rank) %>%
   ungroup() %>%
   mutate(biom=paste0("b",sprintf("%02d",biom)),
