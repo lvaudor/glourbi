d1d6234783c964629a84c74256c05c4cd83df28d
lvaudor <lise.vaudor@ens-lyon.fr>
2024-05-30 12:51:31 +0200

    remove selectA

--- data-raw/prepare_all_cities.R
+++ data-raw/prepare_all_cities.R
@@ -57,10 +57,6 @@ all_cities=all_cities %>%
          clim=as.factor(clim)
          ) %>%
   na.omit() %>%
-
-  mutate(selectA=case_when(City.Code %in% study_areas$City.Code~ TRUE,
-                           TRUE~ FALSE)) %>%
-
   mutate(selection1=case_when(ID %in% selection1_cities$CityCode~ TRUE,
                            TRUE~ FALSE))
 
