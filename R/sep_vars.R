bc455a93e3560578ebcfa638653512595508d3f0
lvaudor <lise.vaudor@ens-lyon.fr>
2024-05-30 11:26:05 +0200

    add selection1 to vars list

--- R/sep_vars.R
+++ R/sep_vars.R
@@ -3,17 +3,17 @@
 #' A function that separates variables in the dataset into 3 groups: identifying (vars_id), descriptive (vars_des), numeric (vars_num)
 #' @param dataset a glourbi dataset with varnames comparable to all_cities
 #' @return a list with elements vars_id, vars_des, vars_cat, vars_num
-#' 
+#'
 #' @export
 #' @examples
 #' sep_vars(all_cities)
 sep_vars <- function(dataset){
-  dataset =dataset %>% 
+  dataset =dataset %>%
     na.omit()
   # Keep only variables used for calculations of PCA
   vars_id="name"
   vars_des=c("Urban.Aggl","ID","Latitude","Longitude",
-             "Continent","Country.Co","City.Code","selectA"
+             "Continent","Country.Co","City.Code","selection1"
              )
   vars_cat=c("clco","biom","clim")
   if("cluster" %in% colnames(dataset)){vars_cat=c(vars_cat,"cluster")}
