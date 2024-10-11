f61e8845680e703c85e0caa6251425bf8ec615a2
Lise Vaudor <lise.vaudor@ens-lyon.fr>
2024-10-11 11:47:26 +0200

    add txt_utils

---
+++ R/txt_utils.R
@@ -0,0 +1,40 @@
+#' Returns txt_page data for given citycode and river
+#' @param thisCityCode the citycode
+#' @param thisRiver the river
+#' @param conn the connection to glourb database
+#' @return
+#' @export
+get_txt_page=function(thisCityCode,
+                      thisRiver,
+                      conn){
+  tib_page=glourbi::get_city_tib(name="txt_page",
+                                 thisCityCode=thisCityCode,
+                                 conn=conn) %>%
+    dplyr::select(river_en,query,position,title,link,domain,trans_snippet,text_en,id)
+
+  if(length(thisRiver)==1){
+    result=tib_page %>%
+      dplyr::filter(river_en==thisRiver)
+  }else{result=tib_page}
+  return(result)
+}
+#' Returns txt_page data for given citycode and river
+#' @param thisCityCode the citycode
+#' @param thisRiver the river
+#' @param conn the connection to glourb database
+#' @return
+#' @export
+get_txt_segment=function(thisCityCode,
+                         thisRiver,
+                         conn){
+
+  tib_segment=glourbi::get_city_tib(name="txt_segment",
+                                    thisCityCode=thisCityCode,
+                                    conn=conn)
+  if(length(thisRiver)==1){
+    result=tib_segment %>%
+        dplyr::filter(river_en==thisRiver)
+  }else{result=tib_segment}
+  return(result)
+
+}
